4414927e70dd34c9f7acbb5029f5f5623f89e664
daantoma <daantoma@hotmail.com>
2020-03-27 21:35:24 -0500

    first commit

---
+++ ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { PersonaRegistroComponent } from './pulsacion/persona-registro/persona-registro.component';
+import { PersonaConsultaComponent } from './pulsacion/persona-consulta/persona-consulta.component';
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [
+  {
+  path: 'personaConsulta',
+  component: PersonaConsultaComponent
+  },
+  {
+    path: 'personaRegistro',
+    component: PersonaRegistroComponent
+  }
+];
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
